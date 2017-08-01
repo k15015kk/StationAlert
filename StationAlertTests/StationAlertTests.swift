4edd3c99ce24bd73acecca1b8cc1cd81b9a0b1e7
k15015kk <k15015kk@macbook-pro-by-ericwifina.local>
2017-08-01 15:15:06 +0900

    Initial Commit

---
+++ StationAlertTests/StationAlertTests.swift
@@ -0,0 +1,36 @@
+//
+//  StationAlertTests.swift
+//  StationAlertTests
+//
+//  Created by k15015kk on 2017/08/01.
+//  Copyright © 2017年 HarukiInoue. All rights reserved.
+//
+
+import XCTest
+@testable import StationAlert
+
+class StationAlertTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
